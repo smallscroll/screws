fc6fb483bec9875492cd85df52020d71f54ab6cf
zyc <crux@waaa.org>
2020-01-17 11:38:07 +0800

    update

--- dailybomb.go
+++ dailybomb.go
@@ -7,51 +7,45 @@ import (
 
 //IDailyBomb ...
 type IDailyBomb interface {
-	Reset(clock [3]int)
 	Defuse()
 }
 
 //NewDailyBomb ...
-func NewDailyBomb(function func(d IDailyBomb), clock [3]int) IDailyBomb {
-	lb := &dailyBomb{
+func NewDailyBomb(function func(), clock [3]int) IDailyBomb {
+	d := &dailyBomb{
 		Powder:  function,
 		Clock:   clock,
 		Defused: make(chan bool),
 	}
-	go lb.ignite()
-	return lb
+	go d.ignite()
+	return d
 }
 
 //dailyBomb ...
 type dailyBomb struct {
-	Powder  func(d IDailyBomb)
+	Powder  func()
 	Clock   [3]int
 	Defused chan bool
 }
 
 //ignite ...
 func (d *dailyBomb) ignite() {
-	d.Powder(d)
+	d.Powder()
 	for {
 		today := time.Now()
 		tomorrow := today.Add(time.Hour * 24)
 		tomorrow = time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), d.Clock[0], d.Clock[1], d.Clock[2], 0, tomorrow.Location())
 		select {
 		case <-d.Defused:
+			log.Println("LoopBomb  Defused")
 			return
 		case <-time.After(tomorrow.Sub(today)):
-			d.Powder(d)
+			d.Powder()
 		}
 	}
 }
 
 //Defuse ...
-func (d *dailyBomb) Reset(clock [3]int) {
-	d.Clock = clock
-}
-
-//Defuse ...
 func (d *dailyBomb) Defuse() {
 	d.Defused <- true
-	log.Println("LoopBomb  Defused")
 }
